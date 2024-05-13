be4cf30fdb830c95359f4659954183dd08c73814
DariaCristiana24 <dariacristiana24@gmail.com>
2024-05-13 17:39:29 +0200

    updated player fall7ng

--- Project-Show-Off/Assets/Scripts/PlayerCamera.cs
+++ Project-Show-Off/Assets/Scripts/PlayerCamera.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField]
     Transform VRCamera;
+    [SerializeField]
+    Transform player;
 
     [SerializeField]
     bool falling;
@@ -18,13 +21,20 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(player.transform.position.y < 0.5)
+        {
+            falling = true;
+        }
+
         if (!falling)
         {
             transform.position = VRCamera.transform.position;
         }
         else
         {
-            transform.position = new Vector3(VRCamera.transform.position.x, transform.position.y - 1, VRCamera.transform.position.z);
+            //transform.position = new Vector3(VRCamera.transform.position.x, transform.position.y - 1, VRCamera.transform.position.z);
+            transform.position = player.transform.position;
+            
         }
         
         transform.rotation = VRCamera.transform.rotation; 
