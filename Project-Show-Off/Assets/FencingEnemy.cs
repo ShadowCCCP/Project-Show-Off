d9bcc9d0154038a23cf9d07b4a6142cfd8a38d43
ShadowCCCP <andreas.degtjarow@web.de>
2024-05-13 15:43:51 +0200

    Finished base for fencing minigame

--- Project-Show-Off/Assets/FencingEnemy.cs
+++ Project-Show-Off/Assets/FencingEnemy.cs
@@ -1,55 +1,127 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class FencingEnemy : MonoBehaviour
 {
-    // This event is triggered when the player gets on the plank...
-    public event Action onTriggerFight;
-    // This event is triggered as soon as a certain animation is finished...
-    public event Action onNextSequence;
+    // This event is triggered as soon as a certain animation is finished
+    // or the player enters the plank for the first time ...
+    public event Action onTriggerSequence;
+    public event Action onStartStagger;
 
     [SerializeField] int attackCount;
-    private int _currentStage;
+    private int _currentAttackCount;
 
     private List<GameObject> _hitPoints = new List<GameObject>();
+    private int _currentHitPoint;
+
+    private bool _initialized;
 
     private void Start()
     {
-        onTriggerFight += TriggerAttack;
-        
+        onTriggerSequence += ManageState;
+        onStartStagger += TriggerStagger;
 
         List<GameObject> gameObjects = new List<GameObject>();
         gameObject.GetChildGameObjects(gameObjects);
 
-        // Get all hitpoints that are children of this object...
+        // Get all hitpoints that are children of this object and hide them...
         foreach (GameObject gameObject in gameObjects)
         {
             if (gameObject.GetComponent<FencingHitPoint>() != null)
             {
                 _hitPoints.Add(gameObject);
+                gameObject.SetActive(false);
             }
         }
     }
 
     private void OnDestroy()
     {
-        onTriggerFight -= TriggerAttack;
+        onTriggerSequence -= TriggerAttack;
+        onStartStagger -= TriggerStagger;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TriggerStagger();
+        }
+    }
+
+    private void ManageState()
+    {
+        if (!_initialized)
+        {
+            // Do the intro animation...
+            
+            _initialized = true;
+            return;
+        }
+
+        TriggerAttack();
+
+        // Increment attackCount and check if game is finished...
+        _currentAttackCount++;
+        if (_currentAttackCount >= attackCount)
+        {
+            // Minigame finished...
+            // !! DO SOMETHING !!
+        }
+    }
+
+    private void TriggerStagger()
+    {
+        int staggerDuration = 5;
+        StartCoroutine(Stagger(staggerDuration));
+    }
+
+    private IEnumerator Stagger(float pWaitTime)
+    {
+        ShowHitPoint();
+        // Do the stagger animation...
+        
+
+        // Loop the animation for this duration...
+        yield return new WaitForSeconds(pWaitTime);
+
+        HideHitPoint();
+        // Transition to next attack...
+        ManageState();
+    }
+
+    private void ShowHitPoint()
+    {
+        // Pick random hitpoint to be visible...
+        _currentHitPoint = UnityEngine.Random.Range(0, _hitPoints.Count);
+        _hitPoints[_currentHitPoint].SetActive(true);
+    }
+
+    private void HideHitPoint()
+    {
+        // Disable currently active hitpoint, if it still exists...
+        if (_hitPoints[_currentHitPoint] != null)
+        {
+            _hitPoints[_currentHitPoint].SetActive(false);
+            _hitPoints.Remove(_hitPoints[_currentHitPoint]);
+        }
     }
 
     private void TriggerAttack()
     {
-        Attack(2);
+        int timeBeforeAttack = 2;
+        StartCoroutine(Attack(timeBeforeAttack));
     }
 
     private IEnumerator Attack(float pWaitTime)
     {
         yield return new WaitForSeconds(pWaitTime);
 
-        
+        // Do the attack animation...
+
     }
 }
