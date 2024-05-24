50fc95313217f1d965f03a2c9690ac1f57ea01b6
ShadowCCCP <andreas.degtjarow@web.de>
2024-05-24 14:21:32 +0200

    Improved painting behaviour

--- Project-Show-Off/Assets/Scripts/DrawScene/ColorPad.cs
+++ Project-Show-Off/Assets/Scripts/DrawScene/ColorPad.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorPad : MonoBehaviour
@@ -5,21 +6,41 @@ public class ColorPad : MonoBehaviour
     [SerializeField] ColorMatcher.Colors color;
     private Transform _lastBrushTouched;
     private MarkerTexture _markerScript;
+
+    List<ContactPoint> contactPoints = new List<ContactPoint>();
  
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Detected");
-        if (collision.transform.CompareTag("BrushTip"))
+        bool valid = false;
+
+        // Get all contactPoints to see who was hit exactly...
+        contactPoints.Clear();
+        collision.GetContacts(contactPoints);
+
+        // Check if brushTip hit the color...
+        foreach (ContactPoint c in contactPoints)
         {
-            Debug.Log("Brush!!!");
-
-            if (_markerScript == null || _lastBrushTouched != collision.transform)
+            if (c.otherCollider.CompareTag("BrushTip"))
             {
-                _lastBrushTouched = collision.transform;
-                _markerScript = _lastBrushTouched.parent.GetComponent<MarkerTexture>();
+                valid = true; 
             }
+            // Continue if BrushTip is one of contactPoints...
+            if (valid) continue;
+        }
 
-            _markerScript.ChangeColor(color);
+        // Get the markerScript and set the color/material of the brush...
+        if (valid) 
+        {
+            _markerScript = collision.transform.GetComponent<MarkerTexture>();
+            if (_markerScript != null)
+            {
+                if (_markerScript == null || _lastBrushTouched != collision.transform)
+                {
+                    _lastBrushTouched = collision.transform;
+                }
+
+                _markerScript.ChangeColor(color);
+            }
         }
     }
 }
