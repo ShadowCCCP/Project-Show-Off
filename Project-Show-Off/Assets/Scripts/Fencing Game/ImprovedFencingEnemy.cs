ebd178d8022dff8762d15746259d77ca26f40fc8
ShadowCCCP <andreas.degtjarow@web.de>
2024-05-30 12:33:23 +0200

    Further worked on ImprovedFencingEnemy (not finished)

--- Project-Show-Off/Assets/Scripts/Fencing Game/ImprovedFencingEnemy.cs
+++ Project-Show-Off/Assets/Scripts/Fencing Game/ImprovedFencingEnemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,14 +13,21 @@ public class ImprovedFencingEnemy : MonoBehaviour
     [SerializeField] float walkDistance = 2.0f;
     [Tooltip("Decides how long it takes for the pirate to cover the walking distance.")]
     [SerializeField] float walkTime = 2.0f;
+    [Tooltip("Decided how many times the player/pirate needs to move in order to win.")]
+    [SerializeField] int stageMaxCount = 2;
 
-    [SerializeField] int stageCount = 2;
+    [SerializeField] float idleWaitTime = 3.0f;
+
+    private const int _maxAttackQueueCount = 3;
+    private int _currentAttackCount;
 
     private Animator _anim;
 
+    // This is to check if either the player or the pirate has reached the end of the plank...
     private int _currentFightStage;
 
     private List<int> _attacksDone = new List<int>();
+    private int _blockCount;
 
     private bool _gotHit;
 
@@ -32,6 +40,8 @@ public class ImprovedFencingEnemy : MonoBehaviour
 
     public void Move(bool pForward = true)
     {
+        _currentState = FencingState.Walk;
+
         if (pForward)
         {
             _anim.SetTrigger("WalkForward");
@@ -52,16 +62,21 @@ public class ImprovedFencingEnemy : MonoBehaviour
                 _anim.SetTrigger("StopWalk");
             });
         }
+
+        TransitionState();
     }
 
     public void Attack()
     {
+        // TODO:
+        // Activate sword trigger collider...
+
         int randomAttack = Random.Range(0, 2);
 
         // If max amount of attacks isn't reached...
-        if (_attacksDone.Count < 3)
+        if (_attacksDone.Count < _currentAttackCount)
         {
-            // Get a random, but valid number...
+            // Get a random, but valid number and trigger animation...
             while (_attacksDone.Contains(randomAttack))
             {
                 randomAttack = Random.Range(0, 2);
@@ -70,21 +85,39 @@ public class ImprovedFencingEnemy : MonoBehaviour
         }
         else
         {
+            CheckAttackOutcome();
+
+            // Increase the amount of attacks done, if max not reached yet...
+            if (_currentAttackCount < _maxAttackQueueCount)
+            {
+                _currentAttackCount++;
+            }
+
+            // Finish attack animation and clear the attackDone list...
             _anim.SetTrigger("FinishedAttack");
+            _attacksDone.Clear();
+        }
+    }
+
+    private void CheckAttackOutcome()
+    {
+        if (_blockCount >= _currentAttackCount)
+        {
+            _anim.SetBool("GotBlocked", true);
         }
     }
 
     private void ProcessCurrentStage()
     {
-        if (_currentFightStage >= stageCount || _currentFightStage <= -stageCount)
+        if (_currentFightStage >= stageMaxCount || _currentFightStage <= -stageMaxCount)
         {
             // Kick the player off the plank...
-            if (_currentFightStage >= stageCount)
+            if (_currentFightStage >= stageMaxCount)
             {
 
             }
             // Pirate falls into water...
-            else if (_currentFightStage <= -stageCount)
+            else if (_currentFightStage <= -stageMaxCount)
             {
                 _anim.SetBool("WaterFall", true);
             }
@@ -93,10 +126,40 @@ public class ImprovedFencingEnemy : MonoBehaviour
         }
     }
 
+    private IEnumerator InitiateAttack()
+    {
+        yield return new WaitForSeconds(idleWaitTime);
+        Attack();
+    }
+
+
+    private void ResetValues()
+    {
+        _anim.SetInteger("Attack", -1);
+        _anim.SetBool("GotBlocked", false);
+        _anim.SetBool("WaterFall", false);
+        _anim.SetBool("StunNotDamaged", false);
+    }
+
+    // Methods to be triggered by animator events...
+    public void TransitionState()
+    {
+        if (_currentState == FencingState.Intro)
+        {
+            _anim.SetTrigger("WalkForward");
+            _currentState = FencingState.Walk;
+            Move();
+        }
+        else if (_currentState == FencingState.Walk)
+        {
+            StartCoroutine(InitiateAttack());
+        }
+    }
+
     // Methods triggered outside this class...
     public void GotBlocked()
     {
-
+        _blockCount++;
     }
 
     public void SideGotHit(SideHit pSide)
