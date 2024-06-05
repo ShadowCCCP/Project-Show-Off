c91cd94df0bb7a0443f613bf58124d1270f037fb
DariaCristiana24 <dariacristiana24@gmail.com>
2024-06-05 12:41:32 +0200

    combined blowtorch with vfx

--- Project-Show-Off/Assets/Scripts/Space/BlowTorch.cs
+++ Project-Show-Off/Assets/Scripts/Space/BlowTorch.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class BlowTorch : MonoBehaviour
 {
+    [SerializeField]
+    ParticleSystem sparks;
+    [SerializeField]
+    ParticleSystem smoke;
 
 
+    private void Start()
+    {
+        sparks.Stop();
+        smoke.Stop();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<WeldableCube>() != null)
+        {
+            sparks.Play();
+            smoke.Play();
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<WeldableCube>() != null)
+        {
+            sparks.Stop();
+            smoke.Stop();
+        }
+    }
 }
